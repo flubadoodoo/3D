73ab4334a66cfb955b21852ddee9c83a6f3d135d
Hamdan <hamdanjaveed@gmail.com>
2013-02-25 22:54:51 -0500

    BUG: tried to implement movement, but something is clearly wrong

--- src/core/Main.java
+++ src/core/Main.java
@@ -18,9 +18,9 @@ public class Main {
 	private static final int WIDTH; // width of the display
 	private static final int HEIGHT; // height of the display
 	private static final int TARGET_FRAME_RATE; // the target frame rate of the program
+	private static final float MOVE_SPEED; // the speed at which the player (really the camera) moves
 	
-	private long lastFrameSystemTime; // the last frame's time according to the system
-	private int deltaTimeFromLastFrame; // the change in time between the current time to the last frame's time
+	private long lastFrameSystemTime; // the last frame's time according to the system	
 	
 	private Camera camera; // a class to encapsulate all camera functionality
 	
@@ -32,6 +32,7 @@ public class Main {
 		WIDTH = 1280;
 		HEIGHT = 720;
 		TARGET_FRAME_RATE = 60;
+		MOVE_SPEED = 0.01f;
 	}
 	
 	/**
@@ -72,7 +73,7 @@ public class Main {
 	 * Initialize the camera and grab the mouse
 	 * */
 	private void initGL() {
-		camera = new Camera(new Vector3f(0, 0, -10), 70.0f, (float) Main.WIDTH / Main.HEIGHT, 0.3f, 1000.0f);
+		camera = new Camera(new Vector3f(0, 0, -10), 70.0f, (float) Main.WIDTH / (float) Main.HEIGHT, 0.3f, 1000.0f);
 		Mouse.setGrabbed(true);
 	}
 	
@@ -88,7 +89,6 @@ public class Main {
 	 * */
 	private void initGLVars() {
 		lastFrameSystemTime = Clock.getTime();
-		deltaTimeFromLastFrame = 1;
 	}
 
 	/**
@@ -96,7 +96,7 @@ public class Main {
 	 * */
 	private void mainLoop() {
 		while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-			// get input
+			pollInput(getDelta());
 			// update scene
 			renderScene();
 			Display.update();
@@ -105,13 +105,68 @@ public class Main {
 	}
 	
 	/**
+	 * Poll the input from the keyboard and the mouse, and move the camera accordingly
+	 * @param delta the delta value, multiply with every movement
+	 * */
+	private void pollInput(int delta) {
+		float distance = MOVE_SPEED * delta;
+		float xDistance = (Keyboard.isKeyDown(Keyboard.KEY_A) != Keyboard.isKeyDown(Keyboard.KEY_D) ? (Keyboard.isKeyDown(Keyboard.KEY_A)) ? -distance : distance : 0f);
+		float yDistance = (Keyboard.isKeyDown(Keyboard.KEY_W) != Keyboard.isKeyDown(Keyboard.KEY_S) ? (Keyboard.isKeyDown(Keyboard.KEY_W)) ? -distance : distance : 0f);
+		camera.move(new Vector3f(xDistance, 0f, yDistance));
+	}
+	
+	/**
 	 * Renders the scene
 	 * */
 	private void renderScene() {
-		deltaTimeFromLastFrame = getDelta();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glLoadIdentity();
 		camera.updateCamera();
+		glBegin (GL_QUADS);
+		{
+			// FrontFace
+			glColor3f (1f, 0f, 0f);
+			glVertex3f (-1, -1, 1);
+			glVertex3f (1, -1, 1);
+			glVertex3f (1, 1, 1);
+			glVertex3f (-1, 1, 1);
+
+			// BackFace
+			glColor3f (0f, 1f, 0f);
+			glVertex3f (-1, -1, -1);
+			glVertex3f (-1, 1, -1);
+			glVertex3f (1, 1, -1);
+			glVertex3f (1, -1, -1);
+
+			// BottomFace
+			glColor3f (0f, 0f, 1f);
+			glVertex3f (-1, -1, -1);
+			glVertex3f (-1, -1, 1);
+			glVertex3f (-1, 1, 1);
+			glVertex3f (-1, 1, -1);
+
+			// TopFace
+			glColor3f (1f, 1f, 0f);
+			glVertex3f (1, -1, -1);
+			glVertex3f (1, -1, 1);
+			glVertex3f (1, 1, 1);
+			glVertex3f (1, 1, -1);
+
+			// LeftFace
+			glColor3f (0f, 1f, 1f);
+			glVertex3f (-1, -1, -1);
+			glVertex3f (1, -1, -1);
+			glVertex3f (1, -1, 1);
+			glVertex3f (-1, -1, 1);
+
+			// Right Face
+			glColor3f (1f, 0f, 1f);
+			glVertex3f (-1, 1, -1);
+			glVertex3f (1, 1, -1);
+			glVertex3f (1, 1, 1);
+			glVertex3f (-1, 1, 1);
+		}
+		glEnd ();
 	}
 	
 	/**
