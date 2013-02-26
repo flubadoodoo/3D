3b9c4f39adf18db79d60280c475c9e23331fc2dc
Hamdan <hamdanjaveed@gmail.com>
2013-02-25 20:23:03 -0500

    added Camera

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,6 +1,18 @@
 package core;
 
+import camera.Camera;
+
 public class Main {
+	
+	private static final int WIDTH;
+	private static final int HEIGHT;
+	
+	private Camera camera;
+	
+	static {
+		WIDTH = 1280;
+		HEIGHT = 720;
+	}
 	
 	public Main() {
 		
